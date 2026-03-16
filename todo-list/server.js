a84ff0ae3ad5ee7a4a08c6b0015509c84da99389
Manya Kumar <manyakumar1205@gmail.com>
2026-03-16 22:12:03 +0530

    Connect to server

--- todo-list/server.js
+++ todo-list/server.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express=require('express')
 const mongoose=require('mongoose')
+const todoRoutes=require('./routes/todos')
 const app=express()
 
 app.use(express.json())
+
+app.use('/api/todos', todoRoutes)
 
 app.get('/', (req,res) => {
   res.send('Todo API is running...')
