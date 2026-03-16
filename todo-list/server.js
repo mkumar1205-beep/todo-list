04ff2b6d30ddbd46d7f10c29fcbabd486135a02c
Manya Kumar <manyakumar1205@gmail.com>
2026-03-16 16:06:11 +0530

    initial setup

---
+++ todo-list/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express=require('express')
+const mongoose=require('mongoose')
+const app=express()
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+  res.send('Todo API is running...')
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected!')
+    app.listen(PORT , () => console.log(`Server running on PORT ${PORT}`))
+  })
+  .catch((err) => {
+    console.log('MongoDB connection failed' , err.message)
+  })
